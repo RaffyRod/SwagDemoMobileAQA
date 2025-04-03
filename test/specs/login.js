7992cfa81f51c6a1e768a5cb6dae715808358e75
RaffyRod <elraffy3@gmail.com>
2025-04-02 23:40:36 -0400

    adding product page and methods

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,10 +1,13 @@
 //import { LoginPage } from '../pageobjects/loginPage.js';
 const { LoginPage } = require('../pageobjects/loginPage.js');
+const { ProductPage } = require('../pageobjects/productPage.js');
 
 describe('Tests', () => {
 	it('login', async () => {
 		const loginPage = new LoginPage();
+		const productPage = new ProductPage();
 		await loginPage.loginProcess('standard_user', 'secret_sauce');
+		await productPage.verifyProductHeader();
 	});
 
 });
