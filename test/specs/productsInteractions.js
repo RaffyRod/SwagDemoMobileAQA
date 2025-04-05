1cb302e9e456a02e1dd3d64378351d7d8b502acd
RaffyRod <elraffy3@gmail.com>
2025-04-05 00:50:48 -0400

    adding second TC

--- test/specs/productsInteractions.js
+++ test/specs/productsInteractions.js
@@ -3,11 +3,12 @@ const { ProductPage } = require('../pageobjects/productPage.js');
 const data = require('../../utils/testData/testdata.json');
 
 describe('Tests', () => {
-	it('[TC-001]: Login Flow', async () => {
+	it('[TC-002] Change Products View', async () => {
 		const loginPage = new LoginPage();
 		const productPage = new ProductPage();
 		await loginPage.loginProcess(data.standardUser, data.standardUserPass);
 		await productPage.verifyProductHeader();
+        await productPage.changeListViews();
 	});
 
 });
